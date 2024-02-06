d83408f0dd5e9c62d106d6c1d9d507c65d9e5183
L.B.Asoftware <enge.sibrahim@gmail.com>
2024-02-06 09:19:19 +0200

    add course files

---
+++ src/main/java/com/example/firstWebApp/controles/courseControler.java
@@ -0,0 +1,20 @@
+package com.example.firstWebApp.controles;
+import com.example.firstWebApp.entities.course;
+import com.example.firstWebApp.services.courseServices;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class courseControler {
+
+    private courseServices courseServices;
+    @PostMapping("/course/addCourse")
+    public @ResponseBody course addCourse(@RequestBody course c)
+    {
+        return courseServices.addCourse(c);
+    }
+}
--- src/main/java/com/example/firstWebApp/services/userServices.java
+++ src/main/java/com/example/firstWebApp/services/userServices.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 public class userServices {
     @Autowired
-    userRepository repository;
+    private userRepository repository;
 
     public user addUser(user u)
     {
