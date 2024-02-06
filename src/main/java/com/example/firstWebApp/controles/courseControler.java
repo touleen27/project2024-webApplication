728b3f36970c5193739e77009e302c93de561c8a
L.B.Asoftware <enge.sibrahim@gmail.com>
2024-02-06 09:26:30 +0200

    add course files add code

--- src/main/java/com/example/firstWebApp/controles/courseControler.java
+++ src/main/java/com/example/firstWebApp/controles/courseControler.java
@@ -1,6 +1,7 @@
 package com.example.firstWebApp.controles;
 import com.example.firstWebApp.entities.course;
 import com.example.firstWebApp.services.courseServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class courseControler {
 
+    @Autowired
     private courseServices courseServices;
     @PostMapping("/course/addCourse")
     public @ResponseBody course addCourse(@RequestBody course c)
