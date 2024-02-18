19df2219e4667279e38b3dba879e3810b9ae9e0e
L.B.Asoftware <enge.sibrahim@gmail.com>
2024-02-18 21:56:32 +0200

    adding login method in 4 places

--- src/main/java/com/example/firstWebApp/controles/courseControler.java
+++ src/main/java/com/example/firstWebApp/controles/courseControler.java
@@ -3,10 +3,7 @@ import com.example.firstWebApp.entities.course;
 import com.example.firstWebApp.services.courseServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Controller
@@ -18,5 +15,10 @@ public class courseControler {
     public @ResponseBody course addCourse(@RequestBody course c)
     {
         return courseServices.addCourse(c);
+    }
+    @GetMapping("/course/findByName/{name}")
+    public @ResponseBody course findCourseByName(@PathVariable(name = "name") String name)
+    {
+        return courseServices.findByName(name);
     }
 }
